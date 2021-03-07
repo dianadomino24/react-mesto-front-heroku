b759efc23140827f5a4eab67291d0460665191cd
Diana Vlasenkova <dianadomino24work@gmail.com>
2021-03-07 21:03:02 +0100

    Updates

---
+++ precache-manifest.ecf87d3cf24f955ed9c457b6778f709d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc92b9ace79978d785f13e704e7984ef",
+    "url": "/react-mesto-front-heroku/index.html"
+  },
+  {
+    "revision": "3c55cb22ce43226dc41d",
+    "url": "/react-mesto-front-heroku/static/css/main.5ebe3da9.chunk.css"
+  },
+  {
+    "revision": "54bb4564334b6136f765",
+    "url": "/react-mesto-front-heroku/static/js/2.5b22ece4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mesto-front-heroku/static/js/2.5b22ece4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c55cb22ce43226dc41d",
+    "url": "/react-mesto-front-heroku/static/js/main.f1b62a48.chunk.js"
+  },
+  {
+    "revision": "9d1ba501164d4e393cba",
+    "url": "/react-mesto-front-heroku/static/js/runtime-main.c2e3fd95.js"
+  },
+  {
+    "revision": "1e365ba4ffeb3bff38e38435eb85086c",
+    "url": "/react-mesto-front-heroku/static/media/add-button.1e365ba4.svg"
+  },
+  {
+    "revision": "4056b612a59cf5b2722bc52744a46b58",
+    "url": "/react-mesto-front-heroku/static/media/close-button.4056b612.svg"
+  },
+  {
+    "revision": "17db2d3c2637b981e46f1263897407e7",
+    "url": "/react-mesto-front-heroku/static/media/edit-button.17db2d3c.svg"
+  },
+  {
+    "revision": "c32299fa3125c41146aa8359b2307523",
+    "url": "/react-mesto-front-heroku/static/media/fail-icon.c32299fa.svg"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/react-mesto-front-heroku/static/media/inter-400.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/react-mesto-front-heroku/static/media/inter-400.3ae6a7d3.woff"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/react-mesto-front-heroku/static/media/inter-700.00189378.woff"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/react-mesto-front-heroku/static/media/inter-700.e7ae9868.woff2"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/react-mesto-front-heroku/static/media/inter-900.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/react-mesto-front-heroku/static/media/inter-900.b90562e0.woff"
+  },
+  {
+    "revision": "b12167400da3396be4b9b6e69ba27f5d",
+    "url": "/react-mesto-front-heroku/static/media/like-button-active.b1216740.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "/react-mesto-front-heroku/static/media/like-button.e7a8906a.svg"
+  },
+  {
+    "revision": "a56cd5e0bdfb2fee54d02325f27eb0b4",
+    "url": "/react-mesto-front-heroku/static/media/logo.a56cd5e0.svg"
+  },
+  {
+    "revision": "7fc4c149eaeefd2cf76f1e1b8ed62cb4",
+    "url": "/react-mesto-front-heroku/static/media/success-icon.7fc4c149.svg"
+  },
+  {
+    "revision": "66807b14703a11264da9f5cfffafe946",
+    "url": "/react-mesto-front-heroku/static/media/trash.66807b14.svg"
+  }
+]);
